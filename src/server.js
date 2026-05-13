c379b73ce7705ce93c391dfc159e7c1a5cad6b12
Yan-Reule <yancreule@gmail.com>
2026-05-13 13:34:51 -0300

    correção de bug: erro de conexão com o banco de dados

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
-require('dotenv').config()
+const path = require('path')
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env')
+})
 const app = require('./app')
 const { testDatabaseConnection } = require("./config/database");
 
